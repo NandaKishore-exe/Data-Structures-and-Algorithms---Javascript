25287ad2de8f24cf8ae670b96910df6350b7190a
nanda <nandakishorelogu@gmail.com>
2026-05-14 21:27:38 +0530

    phase 2 | started solving simple problems

---
+++ phase-2-mathematics/problems/1-countDigits.js
@@ -0,0 +1,15 @@
+function countDigits(n) {
+  let count = 0;
+
+  while (n > 0) {
+    n = Math.floor(n / 10);
+    count++;
+  }
+
+  return count;
+}
+
+console.log(countDigits(123));
+
+// Time Complexity - O(log n)
+// Space Complexity - O(1)
