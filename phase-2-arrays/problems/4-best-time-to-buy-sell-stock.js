229a529fb760754a0024fe8b88ee45511045f64e
nanda <nandakishorelogu@gmail.com>
2026-06-04 08:51:30 +0530

    completed 2 array problems

---
+++ phase-2-arrays/problems/4-best-time-to-buy-sell-stock.js
@@ -0,0 +1,16 @@
+var maxProfit = function (prices) {
+  let minPrice = prices[0];
+  let maxProfit = 0;
+  for (i = 1; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    }
+
+    if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+  return maxProfit;
+};
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
