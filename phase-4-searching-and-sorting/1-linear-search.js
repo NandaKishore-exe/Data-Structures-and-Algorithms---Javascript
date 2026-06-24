d7dc0dd57cd0c77752ffa3437a3c772adba18a03
nanda <nandakishorelogu@gmail.com>
2026-06-24 07:58:22 +0530

    completed till bubble sort

---
+++ phase-4-searching-and-sorting/1-linear-search.js
@@ -0,0 +1,14 @@
+// Find the target element in given array
+
+const arr = [4, 9, 1, 0, 2];
+
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch(arr, 10));
